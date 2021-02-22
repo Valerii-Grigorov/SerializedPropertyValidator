bed10c01f29c60abf291ee8d4e5ca426525969d6
valerii.grigorov <valerii.grigorov@matryoshka.com>
2021-02-22 23:04:50 +0700

    Add SelectionObjectValidateTrigger

--- Editor/ValidateTriggers/PropertyDrawerValidateTrigger.cs
+++ Editor/ValidateTriggers/PropertyDrawerValidateTrigger.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 [CustomPropertyDrawer(typeof(ValidationAttribute), true)]
@@ -8,16 +7,12 @@ public class PropertyDrawerValidateTrigger : PropertyDrawer {
 		EditorGUI.BeginChangeCheck();
 		EditorGUI.PropertyField(position, property, label, true);
 		if ( EditorGUI.EndChangeCheck() ) {
-			ValidateProperty(property, fieldInfo, attribute as ValidationAttribute);
+			Validator.Validate(property, fieldInfo, attribute as ValidationAttribute);
 		}
 	}
  
 	public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
 		return EditorGUI.GetPropertyHeight(property);
-	}
-
-	void ValidateProperty(SerializedProperty property, FieldInfo fieldInfo, ValidationAttribute validationAttribute) {
-		Validator.Validate(property, fieldInfo, validationAttribute);
 	}
 }
 
---
+++ Editor/ValidateTriggers/SelectionObjectValidateTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class SelectionObjectValidateTrigger {
+	[InitializeOnLoadMethod]
+	static void Initialize() {
+		Selection.selectionChanged -= OnSelectionChanged;
+		Selection.selectionChanged += OnSelectionChanged;
+	}
+
+	static void OnSelectionChanged() {
+		switch (Selection.activeObject) {
+			case GameObject gameObject: {
+				foreach ( var component in gameObject.GetComponentsInChildren<Component>(true) ) {
+					Validator.Validate(component);
+				}
+				break;
+			}
+			case ScriptableObject scriptableObject: {
+				Validator.Validate(scriptableObject);
+				break;
+			}
+		}
+	}
+}
